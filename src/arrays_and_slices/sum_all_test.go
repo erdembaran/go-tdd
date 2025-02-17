a818be14fb872336386eb3acfc2d517c3dc7459e
“Baran <109768322+erdembaran@users.noreply.github.com>
2025-02-17 19:03:51 +0300

    chore: update arrays and slices

--- src/arrays_and_slices/sum_all_test.go
+++ src/arrays_and_slices/sum_all_test.go
@@ -6,11 +6,11 @@ import (
 )
 
 func TestSumAll(t *testing.T) {
-	slice1 := []int{2,4,6,8,10}
-	slice2 := []int{2,4,6,8,10,12}
+	slice1 := []int{1,3,5,7}
+	slice2 := []int{2,4,6,8}
 
 	got := SumAll(slice1, slice2)
-	want := []int{30, 42}
+	want := []int{16, 20}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, but want %v", got, want)
