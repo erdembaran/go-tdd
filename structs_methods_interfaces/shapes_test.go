03b9961bf4fa7e7928bb6522f42406bf280d10cf
“Baran <109768322+erdembaran@users.noreply.github.com>
2025-02-19 21:57:07 +0300

    refactor(tests): improve test structure for rectangle and circle area calculations

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -8,9 +8,9 @@ import (
 func TestRectangleArea(t* testing.T) {
 	tests := []struct {
 		name string
-		rect Rectangle
+		rect Rectangle	
 		want float64
-	}{
+	}{	
 		{"Square (8x8)", Rectangle{Width: 8, Height: 8}, 64.00},
 		{"Rectangle (4x6)", Rectangle{Width: 4, Height: 6}, 24.00},
 
@@ -28,13 +28,25 @@ func TestRectangleArea(t* testing.T) {
 }
 
 func TestCircleArea(t *testing.T) {
-	circle := Circle{Radius: 4}
+	tests := []struct {
+		name string
+		circ Circle
+		want float64
+	}{
+		{"Circle1", Circle{Radius: 4}, 50.26},
+		{"Circle2", Circle{Radius: 8},	201.06},
 
-	got := circle.Area()
-	want := math.Pi * 16
+	}
 
-	if got != want {
-		t.Errorf("got %v, but want %v", got, want)
+	const epsilon = 0.01
+
+	for _, tc := range tests {
+		t.Run(tc.name, func (t *testing.T) {
+			got := tc.circ.Area()
+			if math.Abs(got - tc.want) > epsilon {
+                t.Errorf("got %v, want %v", got, tc.want)
+            }
+		})
 	}
 
 
