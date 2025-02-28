330c9fb87d111b61b66dc2c3ea156f779b67f2dd
“Baran <109768322+erdembaran@users.noreply.github.com>
2025-02-28 21:06:34 +0300

    feat: add pointers

---
+++ pointers_and_errors/wallet_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+
+	fmt.Printf("%p", &wallet.balance)
+
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
